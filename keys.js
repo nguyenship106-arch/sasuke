cd847f24b17e57e844d2c0122ab3be8719d54b4e
nguyenship106-arch <nguyenship106@gmail.com>
2026-01-18 11:43:17 +0700

    Update key list by removing old keys and adding new one
    Removed multiple keys that were limited to a single device and added a new key.

--- keys.js
+++ keys.js
@@ -1,42 +1,8 @@
 // ====== UPDATED KEY LIST (chỉ còn 20 key 1 thiết bị ngẫu nhiên) ======
 window.UCHIHA_KEYS = {
   
-  "bi": { 
-    note: "Key chỉ mở cho 1 thiết bị duy nhất", 
-    $device_id: null 
-  },
-  "DevChiCuong": { 
-    note: "Key chỉ mở cho 1 thiết bị duy nhất", 
-    $device_id: null 
-  },
-  "Zani": { 
-    note: "Key chỉ mở cho 1 thiết bị duy nhất", 
-    $device_id: null 
-  },
-  "ZaniCte": { 
-    note: "Key chỉ mở cho 1 thiết bị duy nhất", 
-    $device_id: null 
-  },
-  "Zani9m6": { 
-    note: "Key chỉ mở cho 1 thiết bị duy nhất", 
-    $device_id: null 
-  },
-  "ZaniDev": { 
-    note: "Key chỉ mở cho 1 thiết bị duy nhất", 
-    $device_id: null 
-  },
-  "ChiCuong-Zani": { 
-    note: "Key chỉ mở cho 1 thiết bị duy nhất", 
-    $device_id: null 
-  },
-  "ThienKim2025": { 
-    note: "Key chỉ mở cho 1 thiết bị duy nhất", 
-    $device_id: null 
-  },
-  "MinhHieuDev": { 
-    note: "Key chỉ mở cho 1 thiết bị duy nhất", 
-    $device_id: null 
-  },
+  "bnguyen-10p": { 
+      },
   "PhuongNhiPro": { 
     note: "Key chỉ mở cho 1 thiết bị duy nhất", 
     $device_id: null 
