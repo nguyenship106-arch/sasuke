3dec078fe72ac59f97baba35b9b8badbd311dedd
nguyenship106-arch <nguyenship106@gmail.com>
2026-03-11 21:31:44 +0700

    Rename key 'srorj1h' to 'sangdjtlinh'

--- keys.js
+++ keys.js
@@ -5,7 +5,7 @@ window.UCHIHA_KEYS = {
     note: "Key chỉ mở cho 1 thiết bị duy nhất", 
     $device_id: null 
   },
-  "srorj1h": { 
+  "sangdjtlinh": { 
     note: "Key chỉ mở cho 1 thiết bị duy nhất", 
     $device_id: null 
   },
